e3d88832f086a57093ba6af97974a0cc7e3678f6
Alex Shelkovskiy <alexshelkov@gmail.com>
2022-01-31 01:20:34 +0600

    Add optional test number field

--- src/handler/test/postYup.ts
+++ src/handler/test/postYup.ts
@@ -5,14 +5,21 @@ import { LogTypes, yupService } from '../../text';
 
 const resValid = res.srv(yupService);
 
-const handler: GetHandler<typeof resValid, { message: string; type: LogTypes }, never> = async ({
-  service: { logger, fields: { text } },
+const handler: GetHandler<
+  typeof resValid,
+  { message: string; test?: number; type: LogTypes },
+  never
+> = async ({
+  service: {
+    logger,
+    fields: { text },
+  },
 }) => {
   const { message, type, test } = text;
 
   logger.log(message);
 
-  return ok({ message, type });
+  return ok({ message, type, test });
 };
 
 export const handle = resValid.ok(handler).req();
